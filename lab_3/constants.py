a8a3c9cf3d12644b24a5048a938b5eaff4706417
gar1998 <gareevadel262825@yandex.ru>
2026-05-06 18:35:00 +0400

    Константы

---
+++ lab_3/constants.py
@@ -0,0 +1,13 @@
+REQUIRED_SETTINGS_KEYS = [
+    'initial_file',
+    'encrypted_file',
+    'decrypted_file',
+    'symmetric_key_encrypted',
+    'public_key',
+    'private_key',
+]
+
+SEED_KEY_LENGTH = 16
+SEED_BLOCK_SIZE = 16
+
+RSA_KEY_SIZE = 2048
