f058c8340dfaee35ac936a5cef306bf1e2c8eb56
gar1998 <gareevadel262825@yandex.ru>
2026-05-11 22:50:00 +0400

    Убрал промежуточные проверки

--- lab_3/management.py
+++ lab_3/management.py
@@ -40,25 +40,3 @@ def load_settings(settings_file : str = "settings.json")->tuple[dict[str, str] |
 
     return settings, None
 
-if __name__ == "__main__":
-    settings_file = "test_settings.json"
-
-    settings_paths = {
-        "initial_file": "data/input.txt",
-        "encrypted_file": "data/encrypted.txt",
-        "decrypted_file": "data/decrypted.txt",
-        "symmetric_key_encrypted": "keys/seed_encrypted.txt",
-        "public_key": "keys/public.pem",
-        "private_key": "keys/private.pem"
-    }
-
-    try:
-        with open(settings_file, 'w', encoding='utf-8') as f:
-            json.dump(settings_paths, f, indent=4)
-        print(f"Создан фиктивный файл настроек: {settings_file}")
-    except Exception as e:
-        print(f"Ошибка при создании файла настроек: {e}")
-
-    if os.path.exists(settings_file):
-        os.remove(settings_file)
-        print(f"Удален {settings_file}")
